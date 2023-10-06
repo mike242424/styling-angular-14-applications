82257923a20ad36da8962919e0f0f2047ade7c17
mike242424 <mikebyers24@gmail.com>
2023-10-06 16:29:26 -0400

    linking from external file

--- src/app/app-nav.component.ts
+++ src/app/app-nav.component.ts
@@ -3,31 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-nav',
   template: `
-    <style>
-      div {
-        background: #2a9fbc;
-        border-radius: 0.5em;
-        margin: 1.5em 0;
-        padding: 1em 1.2em;
-      }
-
-      ul {
-        display: flex;
-        list-style: none;
-        margin: 0;
-        padding: 0;
-      }
-
-      a {
-        color: #fff;
-        padding: 0 1em;
-        text-decoration: none;
-      }
-
-      a:hover {
-        text-decoration: underline;
-      }
-    </style>
+    <link rel="stylesheet" href="app-nav.component.css" />
     <div>
       <ul>
         <li>
