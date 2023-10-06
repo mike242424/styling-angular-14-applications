9c73dea29ab1bca4487340fda1031ffe17510da7
mike242424 <mikebyers24@gmail.com>
2023-10-06 16:11:53 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+      <header>
+          <h1>Styling Angular Applications</h1>
+          <em>For Modern Angular Applications</em>
+      </header>
+      <div>
+          <app-nav></app-nav>
+      </div>
+  `
 })
+
 export class AppComponent {
-  title = 'styling-angular-14-applications';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule }        from '@angular/core';
+import { BrowserModule }   from '@angular/platform-browser';
+import { AppComponent }    from './app.component';
+import { AppNavComponent } from './app-nav.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
+  imports:      [ BrowserModule ],
+  declarations: [ 
+      AppComponent,
+      AppNavComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
