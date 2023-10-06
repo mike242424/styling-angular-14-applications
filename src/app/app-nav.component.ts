dd1ef6312b9c0b52787980d6cfe087b82d49a98f
mike242424 <mikebyers24@gmail.com>
2023-10-06 16:21:23 -0400

    stykes in the template

--- src/app/app-nav.component.ts
+++ src/app/app-nav.component.ts
@@ -1,54 +1,49 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-    selector: 'app-nav',
-    encapsulation: ViewEncapsulation.ShadowDom,
-    template: `
-        <div>
-            <ul>
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">Prerequisites</a>
-                </li>
-                <li>
-                    <a href="#">Modules</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-            </ul>
-        </div>
-    `,
-    styles: [`
+  selector: 'app-nav',
+  template: `
+    <style>
+      div {
+        background: #2a9fbc;
+        border-radius: 0.5em;
+        margin: 1.5em 0;
+        padding: 1em 1.2em;
+      }
 
-        div {
-            background: #2A9FBC;
-            border-radius: 0.5em;
-            margin: 1.5em 0;
-            padding: 1em 1.2em;
-        }
+      ul {
+        display: flex;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+      }
 
-        ul {
-            display: flex;
-            list-style: none;
-            margin: 0;
-            padding: 0;
-        }
+      a {
+        color: #fff;
+        padding: 0 1em;
+        text-decoration: none;
+      }
 
-        a {
-            color: #fff;
-            padding: 0 1em;
-            text-decoration: none;
-        }
-
-        a:hover {
-            text-decoration: underline;
-        }
-
-    `]
+      a:hover {
+        text-decoration: underline;
+      }
+    </style>
+    <div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Prerequisites</a>
+        </li>
+        <li>
+          <a href="#">Modules</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+    </div>
+  `,
 })
-
-export class AppNavComponent {
-}
+export class AppNavComponent {}
